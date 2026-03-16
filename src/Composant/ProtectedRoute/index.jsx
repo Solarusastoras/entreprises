aa082d6cb011d0059cf619d2b6cd5baba9ee16d6
Solarus <micformweb@gmail.com>
2026-03-16 10:13:50 +0100

    up

--- src/Composant/ProtectedRoute/index.jsx
+++ src/Composant/ProtectedRoute/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../../Utils/hooks/useAuth";
+import { useAuth } from "../../Utils/hooks/useAuth.js";
 
 export default function ProtectedRoute({ children }) {
   const { estConnecte, chargement } = useAuth();
